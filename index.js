d80c9d93593170dbb9a6353f71a6aab42ee7d2de
Giovana <giamorim2101@gmail.com>
2024-11-05 08:18:46 -0300

    atualizando meu repositorio com o final da aula que não participei

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hello, world!');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// ROTAS
+app.get('/', (req, res) => {
+    res.send("Chamada ao recurso realizada com sucesso");
+});
+
+// retornar todos os usuários
+app.get('/users', (req, res) => {
+    res.send("Aqui vou retornar todos os usuários do sistema")
+});
+
+app.get('/user/:id', (req, res) => {
+    res.send(req.params.id)
+});
+
+app.post('/rotapost', (req, res) => {
+    res.send("Chamada ao recurso usando o post realizada com sucesso");
+});
+
+
+app.listen(PORT, () => {
+    console.log("Servidor aguardando requisições");
+});
